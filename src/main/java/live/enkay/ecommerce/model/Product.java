67360a84d2c3b77b171bde485716cbbe42ec1b00
eNKay <nguyenkhai24082408@gmail.com>
2024-09-19 20:23:49 +0700

    feat: add Product entity
    Implement Product entity with fields for ID, name, category, price, weight, description, and image name, using JPA annotations.

---
+++ src/main/java/live/enkay/ecommerce/model/Product.java
@@ -0,0 +1,28 @@
+package live.enkay.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "category_id")
+  private Category category;
+
+  private double price;
+  private double weight;
+  private String description;
+  private String imageName;
+}
