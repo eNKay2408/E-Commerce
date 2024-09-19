cc290d24ced3b508a3d9732183a560ff82adfd39
eNKay <nguyenkhai24082408@gmail.com>
2024-09-20 01:46:59 +0700

    feat: add CategoryRepository
    Implement CategoryRepository interface extending JpaRepository for Category entity.

---
+++ src/main/java/live/enkay/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package live.enkay.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import live.enkay.ecommerce.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
