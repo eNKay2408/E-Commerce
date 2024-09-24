63c0c15f928617587f8eb3b3bebefa54d42e1285
eNKay <nguyenkhai24082408@gmail.com>
2024-09-25 00:42:10 +0700

    feat: update environment variables
    Load environment variables for MySQL and Cloudinary  configurations.

--- src/main/java/live/enkay/ecommerce/EcommerceApplication.java
+++ src/main/java/live/enkay/ecommerce/EcommerceApplication.java
@@ -12,10 +12,14 @@ public class EcommerceApplication {
 		Dotenv dotenv = Dotenv.load();
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
 		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
 		System.setProperty("STRIPE_SECRET_KEY", dotenv.get("STRIPE_SECRET_KEY"));
 		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
 
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
